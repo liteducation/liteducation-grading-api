104580b418596c70fbbc08e99f74e54582d72124
Ngoc Nguyen <duyngoc.nguyen2001@gmail.com>
2024-06-13 00:58:52 +0700

    share service

--- src/api.module.ts
+++ src/api.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ShareModule } from '@app/share';
 import { OpenAiModule } from './modules/openai/openai.module';
+import { ClassModule } from './modules/class/class.module';
+import { AssignmentModule } from './modules/assignment/assignment.module';
+import { SubmissionModule } from './modules/submission/submission.module';
 
 @Module({
-  imports: [ShareModule, OpenAiModule],
+  imports: [ShareModule, ClassModule, AssignmentModule, SubmissionModule, OpenAiModule],
   controllers: [ApiController],
 })
-export class ApiModule {}
+export class ApiModule { }
